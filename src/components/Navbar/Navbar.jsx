e2f17bdfb1acf3b47f1be55eb84b4db0727ca300
TheChunkyPumpkinhead <chriscampos@remainobscure.com>
2025-03-21 22:06:19 -0700

    Updated UI

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import "./Navbar.css";
+import logo from "../../assets/logo.png";
+import search_icon from "../../assets/logo.png";
 
 const Navbar = () => {
-  return <div className="Navbar">Navbar</div>;
+  return (
+    <div className="Navbar">
+      <div className="navbar-left">
+        <img src="{logo}" alt="" />
+        <ul>
+          <li>Home</li>
+          <li>Tv Shows</li>
+          <li>Movies</li>
+          <li>New & Popular</li>
+          <li>My List </li>
+          <li>Browse by Languges </li>
+        </ul>
+      </div>
+      <div className="navbar-right">
+        <img src={search_icon} alt="" className="icons" />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
